6fb6c45dabcc6a280af6a1e34dbacd7ba7f47464
Chris Markle <christofermarkle@gmail.com>
2016-11-13 18:22:51 -0800

    Created HighlightObject script

--- Assets/Scripts/ClickToMove.cs
+++ Assets/Scripts/ClickToMove.cs
@@ -1,15 +1,11 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class ClickToMove : MonoBehaviour {
 	
 	private NavMeshAgent navMeshAgent;
 	private int clickMask;
-	private bool walking;
-	private bool objectClicked;
-	private Transform highlightedObject;
-	public MouseOnObject mouseOverObject;
-	public DisplayText displayText;
 
 	// Use this for initialization
 	void Start () 
@@ -23,41 +19,14 @@ public class ClickToMove : MonoBehaviour {
 	{
 		RaycastHit hit;
 		Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
-		
-		/*if (Input.GetMouseButtonUp(0))
+
+
+		if (Input.GetMouseButtonUp (0)) 
 		{
-			if (Physics.Raycast (ray, out hit, 100, clickMask))
-				
-				if (hit.collider.CompareTag ("Object")) 
-				{
-					highlightedObject = hit.transform;
-					objectClicked = true;
-					mouseOverObject.OnMouseDown();
-				}
-				
-			else
-				{
-					walking = true;
-					objectClicked = false;
-					navMeshAgent.destination = hit.point;
-				}*/
-
-		if (Input.GetMouseButtonDown(0))
-		{
-			if (Physics.Raycast (ray, out hit, 100, clickMask))
-
-				if (hit.collider.CompareTag ("Object")) 
-				{
-					mouseOverObject.OnMouseDown();
-				}
-
-				else
-				{
-					navMeshAgent.destination = hit.point;
-				}
-		
+			if (Physics.Raycast (ray, out hit, 100, clickMask)) 
+			{	
+				navMeshAgent.SetDestination(hit.point);
+			}
 		}
-		
 	}
 }
-//}
---
+++ Assets/Scripts/ObjectHighlight.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ObjectHighlight : MonoBehaviour {
+
+	public string objectName;
+
+	private Color startColor;
+	private bool displayObjectName = false;
+
+	void OnGUI() {
+
+		DisplayName();
+	
+	}
+
+	void OnMouseEnter() {
+		startColor = GetComponent<Renderer>().material.color;
+		GetComponent<Renderer>().material.color = Color.white;
+		displayObjectName = true;
+		Debug.Log ("Mouse Over " + name);
+	}
+
+	void OnMouseExit(){
+	
+		GetComponent<Renderer>().material.color = startColor;
+		displayObjectName = false;
+	}
+
+	public void DisplayName(){
+	
+		if (displayObjectName) {
+		
+			GUI.Box (new Rect (Event.current.mousePosition.x -155, Event.current.mousePosition.y, 150, 25), objectName);
+		}
+	}
+
+}
