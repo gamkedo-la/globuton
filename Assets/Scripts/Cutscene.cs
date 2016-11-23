b94a67b49e779a7227fb1a25121bdbe7c46557da
Chris Markle <christofermarkle@gmail.com>
2016-11-22 20:53:36 -0800

    Added Cutscene system for intro.

---
+++ Assets/Scripts/Cutscene.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Cutscene : MonoBehaviour {
+	private IEnumerator midPlaying = null;
+	private Vector3 returnToPos;
+	private Quaternion returnToRot;
+	public Transform[] cameraSteps;
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.C) && midPlaying == null) {
+			midPlaying = playCutscene();
+			StartCoroutine (midPlaying);
+		}			
+		if (Input.GetKeyDown (KeyCode.V)) {
+			EndCutscene();
+		}
+	}
+	private void EndCutscene(){
+		if (midPlaying != null) {
+			StopCoroutine (midPlaying);
+			Camera.main.transform.position = returnToPos;
+			Camera.main.transform.rotation = returnToRot;
+			midPlaying = null;
+		}
+	}
+
+	private IEnumerator playCutscene(){
+		returnToPos = Camera.main.transform.position;
+		returnToRot = Camera.main.transform.rotation;
+		for (int i = 0; i < cameraSteps.Length; i++) {
+			Camera.main.transform.position = cameraSteps [i].position;
+			Camera.main.transform.rotation = cameraSteps [i].rotation;
+			yield return new WaitForSeconds (2.0f);
+		}
+		EndCutscene();
+	}
+}
