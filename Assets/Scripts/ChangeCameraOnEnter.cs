465552963f9d21cc64f81a7ee08aca97b0c4ee85
Reid Alford <rmalford2007@gmail.com>
2016-12-04 11:10:58 -0700

    Bug fix - Camera transitions sometimes incorrect
    In conditions where there are multiple overlapping camera position
    triggers, there was a bug that chooses the last entered trigger as the
    camera position. Sometimes this is wrong, added a list of camera
    positions we are currently triggering, on triggerExit removing the
    camera position we are leaving, and setting a new one to the last one in
    the list.

--- Assets/Scripts/ChangeCameraOnEnter.cs
+++ Assets/Scripts/ChangeCameraOnEnter.cs
@@ -1,15 +1,33 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
-public class ChangeCameraOnEnter : MonoBehaviour {
-	Transform camPos;
+public class ChangeCameraOnEnter : MonoBehaviour
+{
 
-	void Start(){
-		camPos = transform.GetChild (0);
-	}
-	void OnTriggerEnter (Collider other){
-		Debug.Log("Collision entered");
-		Camera.main.transform.position = camPos.position;
-		Camera.main.transform.rotation = camPos.rotation;
-	}
+    private static List<Transform> triggeredCameraAreas = new List<Transform>();
+    Transform camPos;
+
+    void Start()
+    {
+        camPos = transform.GetChild(0);
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        triggeredCameraAreas.Add(camPos);
+        //Debug.Log("Collision entered" + gameObject.name.ToString());
+        Camera.main.transform.position = camPos.position;
+        Camera.main.transform.rotation = camPos.rotation;
+    }
+    void OnTriggerExit(Collider other)
+    {
+        triggeredCameraAreas.Remove(camPos);
+        if (triggeredCameraAreas.Count > 0)
+        {
+            Camera.main.transform.position = triggeredCameraAreas[triggeredCameraAreas.Count - 1].position;
+            Camera.main.transform.rotation = triggeredCameraAreas[triggeredCameraAreas.Count - 1].rotation;
+        }
+        //Debug.Log("Collision exit" + gameObject.name.ToString() + " cams left: " + triggeredCameraAreas.Count.ToString());
+    }
+
 }
