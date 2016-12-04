0841c5c79f8efde31cfa95d910106b06d5eb79a0
Reid Alford <rmalford2007@gmail.com>
2016-12-04 03:42:16 -0700

    Add move to and interact functionality
    Works for objects to pick up, as well as activating dialogues when in
    range. Range is currently set on Boxii's ClickToMove script. Distance of
    4.5f seems to work best. If you want have different interact ranges for
    different clickable objects, need to move the interact distance variable
    to a script that resides on the clicked object.

--- Assets/Scripts/ClickToMove.cs
+++ Assets/Scripts/ClickToMove.cs
@@ -8,13 +8,16 @@ public class ClickToMove : MonoBehaviour {
 	private int clickMask;
 	private ObjectHighlight mousedOverCurrently = null;
 	private InventorySystem inventory;
+    private RaycastHit interactHit; //raycast info for object clicked on, but out of range
+    private bool moveToInteract = false; //flag to know if we are moving to interact with something
+    public float interactRange = 4.5f; //Range at which this object can interact with clicked objects, if out of range, move to it then interact
 
     // Use this for initialization
     void Start () {
 		clickMask = ~LayerMask.GetMask ("Camera Trigger");
 		navMeshAgent = GetComponent<NavMeshAgent>();
 		inventory = GetComponent<InventorySystem>();
-
+        moveToInteract = false;
     }
 
 	void OnGUI() {
@@ -25,30 +28,92 @@ public class ClickToMove : MonoBehaviour {
         
 	}
 
+    bool IsInInteractRange(Vector3 a, Vector3 b)
+    {
+        a.y = 0; //ignoring height distance of objects, remove this if you want to require 3d distance
+        b.y = 0; //ignoring height distance of objects, remove this if you want to require 3d distance
+        //Debug.Log(Vector3.Distance(a, b).ToString());
+        return Vector3.Distance(a, b) < interactRange;
+    }
+
+    //Will do an interaction based on if it can be picked up (GiveObject script) or if it has dialogueSpawner script
+    void DoInteract(GameObject interactObject)
+    {
+       
+        if (interactObject != null)
+        {
+            GiveObject goScript = interactObject.GetComponent<GiveObject>();
+            if (goScript != null)
+            {
+                inventory.GiveItem((int)goScript.whichItem);
+                Destroy(goScript.gameObject);
+            }
+            else if (interactObject.tag == "Object")
+            {
+                Debug.Log("You clicked on it");
+                //check if it has a dialogue spawner, if so, call it's OnClick
+                DialogueSpawner showNarrative = interactObject.GetComponent<DialogueSpawner>();
+                if (showNarrative != null)
+                {
+                    //has a dialogue spawner
+                    showNarrative.OnClick();
+                }
+            }   
+        }
+    }
+
+    IEnumerator PollInteractDistance()
+    {
+        do
+        {
+            yield return new WaitForSeconds(1.0f); //every second poll the distance, if in range, interact and get out
+            
+            if (interactHit.transform != null && IsInInteractRange(transform.position, interactHit.transform.position))
+            {
+                navMeshAgent.SetDestination(transform.position);
+                DoInteract(interactHit.collider.gameObject);
+                moveToInteract = false;
+                break;
+            }
+        } while (moveToInteract);
+        yield return null;
+    }
+
 	// Update is called once per frame
 	void Update () {
 		RaycastHit hit;
 		Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
         
-
 		if (Physics.Raycast (ray, out hit, 100, clickMask)) {	
 			if (Input.GetMouseButtonUp (0)) {
-				GiveObject goScript = hit.collider.GetComponent<GiveObject>();
-				if (goScript != null) {
-					inventory.GiveItem((int)goScript.whichItem);
-					Destroy (goScript.gameObject);
-				} else if (hit.collider.tag == "Object") {
-					Debug.Log("You clicked on it");
 
-                    //Object is clicked, check if it has a dialogue spawner, if so, call it's OnClick
-                    DialogueSpawner showNarrative = hit.collider.GetComponent<DialogueSpawner>();
-                    if(showNarrative != null)
+                //If currently doing move to interact, interupt it early, and stop the coroutine that is polling distance
+                if (moveToInteract)
+                {
+                    moveToInteract = false;
+                    StopCoroutine(PollInteractDistance());
+                }
+
+                GiveObject goScript = hit.collider.GetComponent<GiveObject>();
+                if (goScript != null || hit.collider.tag == "Object")
+                {
+                    //We have clicked on object that has dialogue or can be picked up, check if it is in range
+                    if (IsInInteractRange(transform.position, hit.transform.position))
                     {
-                        //has a dialogue spawner
-                        showNarrative.OnClick();
+                        //In range, call the DoInteract function
+                        DoInteract(hit.collider.gameObject);
                     }
-
-				} else {
+                    else   //not in range, start moving towards it, try picking it up again after a short delay
+                    {
+                        //Set move to destination, Start co routine to check again soon
+                        interactHit = hit; //store what we clicked on, so we can check it again later
+                        moveToInteract = true;
+                        StartCoroutine(PollInteractDistance()); //start polling distance
+                        navMeshAgent.SetDestination(hit.point);
+                    }
+                }
+                else   
+                { 
 					navMeshAgent.SetDestination (hit.point);
 				} 
 			} // end GetMouseButtonUp
