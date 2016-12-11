28b13973d8612ac9729b719cb19e219aac1bd593
Chris DeLeon <gamkedo@gmail.com>
2016-12-10 22:52:16 -0800

    Security camera model + dropped into scene

---
+++ Assets/SecurityCamera.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SecurityCamera : MonoBehaviour {
+
+	// Update is called once per frame
+	void Update () {
+		transform.rotation =
+			Quaternion.AngleAxis(-90, Vector3.up) *
+			Quaternion.AngleAxis(-60, Vector3.right) *
+			Quaternion.AngleAxis(180+ Mathf.Cos(Time.time)*30.0f, Vector3.forward);
+	}
+}
