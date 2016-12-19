5c61994397f454c3ac3bfa67602855158fde1efa
Chris Markle <christofermarkle@gmail.com>
2016-12-18 23:31:42 -0800

    Added plaques (and some lights for them) around the orphanage, camera triggers

---
+++ Assets/Scripts/DoorTeleport.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DoorTeleport : MonoBehaviour {
+	public Transform teleportTo;
+
+	void OnClickEnter(Collider col){
+		if(col.GetComponent<Collider>().gameObject.tag == "Teleporter"){
+			transform.position = teleportTo.position;
+			transform.rotation = teleportTo.rotation;
+		}		
+	}
+}
