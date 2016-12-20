badbd9bf819ba87e026308acadeb4c13a0eab7ce
Chris Markle <christofermarkle@gmail.com>
2016-12-19 22:50:50 -0800

    minor changes

--- Assets/Scripts/DoorTeleport.cs
+++ Assets/Scripts/DoorTeleport.cs
@@ -4,10 +4,4 @@ using System.Collections;
 public class DoorTeleport : MonoBehaviour {
 	public Transform teleportTo;
 
-	/*void OnClickEnter(Collider col){
-		if(col.GetComponent<Collider>()){
-			transform.position = teleportTo.position;
-			transform.rotation = teleportTo.rotation;
-		}		
-	}*/
 }
