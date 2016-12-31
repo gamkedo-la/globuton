caace842492ad41abdaae4eafc422160541ab6fa
Chris Markle <christofermarkle@gmail.com>
2016-12-31 14:40:38 -0800

    Added script to open Storage Room door after Keycode item is used on Keypad. However, InventorySystem.cs not allowing all items to appear

---
+++ Assets/Scripts/StorageUnlockAndOpen.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StorageUnlockAndOpen : MonoBehaviour {
+    private bool hasOpenedYet = false;
+    public Transform slideTo;
+
+    public void openNow()
+    {
+        if (hasOpenedYet == false)
+        {
+            Debug.Log("Play 'door slide' sound now");
+            transform.position = slideTo.position;
+            hasOpenedYet = true;
+        }
+    }
+}
