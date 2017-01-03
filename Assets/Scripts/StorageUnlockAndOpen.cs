976c162e317c772df4cb48af82332fa847ed2b36
Chris Markle <christofermarkle@gmail.com>
2017-01-02 20:30:17 -0800

    Minor edits to props

--- Assets/Scripts/StorageUnlockAndOpen.cs
+++ Assets/Scripts/StorageUnlockAndOpen.cs
@@ -5,10 +5,8 @@ public class StorageUnlockAndOpen : MonoBehaviour {
     private bool hasOpenedYet = false;
     public Transform slideTo;
 
-    public void openNow()
-    {
-        if (hasOpenedYet == false)
-        {
+    public void openNow(){
+        if (hasOpenedYet == false){
             Debug.Log("Play 'door slide' sound now");
             transform.position = slideTo.position;
             hasOpenedYet = true;
