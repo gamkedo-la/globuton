def1942cb7498d51c11e13166b9ae1696037ba0c
Chris Markle <christofermarkle@gmail.com>
2017-01-05 13:34:16 -0800

    Adjusted prop materials, fixed Storage Door opening, added Timeout Cubie eyes

---
+++ Assets/Scripts/CodeSlideUnderDoor.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CodeSlideUnderDoor : MonoBehaviour
+{
+    private bool hasSlidYet = false;
+    public Transform slideTo;
+
+    public void slideNow()
+    {
+        if (hasSlidYet == false)
+        {
+            Debug.Log("Play slide sound now");
+            transform.position = slideTo.position;
+            hasSlidYet = true;
+        }
+    }
+}
