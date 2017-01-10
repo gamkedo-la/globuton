3e25f8eed1a46d7483b98549a980316fa8cf00a0
Chris Markle <christofermarkle@gmail.com>
2017-01-09 20:08:45 -0800

    Fixed stupid navmesh

---
+++ Assets/Scripts/CheckedTrashCanAndFoundItem.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StorageUnlockAndOpen : MonoBehaviour {
+    private bool hasOpenedYet = false;
+    public Transform slideTo;
+
+    public void openNow(){
+        if (hasOpenedYet == false){
+            Debug.Log("Play 'door slide' sound now");
+            transform.position = slideTo.position;
+            hasOpenedYet = true;
+        }
+    }
+}
