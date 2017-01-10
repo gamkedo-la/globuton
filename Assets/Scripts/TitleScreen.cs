454e0c42149aa490e5c4175b65be0d6b767ffbc6
oasisrim <oasisrim@ORMBP15.local>
2017-01-09 21:45:26 -0500

    Added title screen font and title screen script.

---
+++ Assets/Scripts/TitleScreen.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class TitleScreen : MonoBehaviour 
+{
+	public GameObject m_titleScreenImage;
+	public Button m_startGameButton;
+
+	// Use this for initialization
+	void Start () 
+	{
+		m_titleScreenImage.SetActive (true);
+		m_startGameButton.onClick.AddListener (StartButtonClick);
+	}
+	
+	void StartButtonClick()
+	{
+		m_titleScreenImage.SetActive (false);
+	}
+}
