92a4b2ac8911bfcd58b9becf1c8d865cf41c15be
Chris DeLeon <gamkedo@gmail.com>
2017-01-13 22:27:17 -0800

    First take on a camera interpolation script (no scene changes made, hook ups handled or worked around in-code)

---
+++ Assets/Scripts/CameraGoalChaser.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraGoalChaser : MonoBehaviour {
+	public static Vector3 wasPos;
+	public static Quaternion wasRot;
+	public static Vector3 goalPos;
+	public static Quaternion goalRot;
+	public static float timeWhen;
+
+	public static Cutscene cutsceneMain;
+
+	void Awake() {
+		GameObject globalGO = GameObject.Find("Global");
+		cutsceneMain = globalGO.GetComponent<Cutscene>();
+	}
+
+	// Use this for initialization
+	void Start () {
+		timeWhen = Time.time;
+
+		wasPos = goalPos = Camera.main.transform.position;
+
+		wasRot = goalRot = Camera.main.transform.rotation;	
+	}
+
+	public static void goalSet(Vector3 pos, Quaternion rot) {
+		wasPos = Camera.main.transform.position;
+
+		wasRot = Camera.main.transform.rotation;	
+
+		timeWhen = Time.time;
+		goalPos = pos;
+		goalRot = rot;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(cutsceneMain.isActiveAndEnabled && cutsceneMain.isPlaying()) {
+			return;
+		}
+		float interpolate = Time.time - timeWhen;
+		if(interpolate > 1.0f) {
+			interpolate = 1.0f;
+		}
+
+		Camera.main.transform.position = Vector3.Lerp(wasPos,
+			goalPos,interpolate);
+
+		Camera.main.transform.rotation = Quaternion.Slerp(wasRot,
+			goalRot,interpolate);	
+	}
+}
