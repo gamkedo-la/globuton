cb39542cc6a2e2b687a9ad84fcce619882c86f54
Chris DeLeon <gamkedo@gmail.com>
2017-01-13 21:08:58 -0800

    Title screen proportion/rescaling implemented, added credits (initial pass) and exit button + escape key hookup

---
+++ Assets/EscToQuit.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EscToQuit : MonoBehaviour {
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.Escape)) {
+			Application.Quit();
+		}
+	}
+
+	public void QuitButton() {
+		Application.Quit();
+	}
+}
--- Assets/Scripts/TitleScreen.cs
+++ Assets/Scripts/TitleScreen.cs
@@ -5,7 +5,6 @@ using UnityEngine.UI;
 public class TitleScreen : MonoBehaviour 
 {
 	public GameObject m_titleScreenImage;
-	public Button m_startGameButton;
     private Cutscene m_cutscene;
 
 	// Use this for initialization
@@ -13,10 +12,9 @@ public class TitleScreen : MonoBehaviour
 	{
         m_cutscene = GetComponent<Cutscene>();
 		m_titleScreenImage.SetActive (true);
-		m_startGameButton.onClick.AddListener (StartButtonClick);
 	}
 	
-	void StartButtonClick()
+	public void StartButtonClick()
 	{
         m_cutscene.enabled = true;
 		m_titleScreenImage.SetActive (false);
