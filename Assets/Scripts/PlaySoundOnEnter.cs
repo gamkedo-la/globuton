de0ab03591ede77512e03291f64cbd9e6d6952fc
oasisrim <oasisrim@gmail.com>
2017-01-16 23:09:11 -0500

    Added "Flies" particle effect above food, added trigger system to enable/disable fly sounds, changed around some Sound Mgr code.

--- Assets/Scripts/PlaySoundOnEnter.cs
+++ Assets/Scripts/PlaySoundOnEnter.cs
@@ -4,10 +4,29 @@ using System.Collections;
 public class PlaySoundOnEnter : MonoBehaviour 
 {
     public AudioClip soundToPlay;
+    public bool isLooping;
+
     void OnTriggerEnter()
     {
-        SoundManager.instance.PlaySingle(soundToPlay);
-        Debug.Log("You are at the main door!"); 
+        if (this.isLooping)
+        {
+            SoundManager.instance.PlayLoop(soundToPlay);
+        }
+        else
+        {
+            SoundManager.instance.PlaySingle(soundToPlay);
+        }
     }
 
+    void OnTriggerExit()
+    {
+        if (this.isLooping)
+        {
+            SoundManager.instance.StopLoop(soundToPlay);
+        }
+        else
+        {
+            SoundManager.instance.StopSingle(soundToPlay);
+        }
+    }
 }
--- Assets/Scripts/SoundManager.cs
+++ Assets/Scripts/SoundManager.cs
@@ -3,7 +3,8 @@ using System.Collections;
 
 public class SoundManager : MonoBehaviour
 {
-    public AudioSource m_sfxSource;
+    public AudioSource m_sfxSingleSource;
+    public AudioSource m_sfxLoopSource;
     public AudioSource m_musicSource;
     public AudioSource m_securityCamSource;
     public static SoundManager instance = null;
@@ -11,6 +12,7 @@ public class SoundManager : MonoBehaviour
     public AudioClip m_stingPuzzle;
     public AudioClip m_stingPickup;
     public AudioClip m_doorOpen;
+    public AudioClip m_flies;
 
     void Awake()
     {
@@ -26,9 +28,28 @@ public class SoundManager : MonoBehaviour
 
     public void PlaySingle(AudioClip clip)
     {
-        m_sfxSource.loop = false;
-        m_sfxSource.clip = clip;
-        m_sfxSource.Play();
+        m_sfxSingleSource.loop = false;
+        m_sfxSingleSource.clip = clip;
+        m_sfxSingleSource.Play();
+    }
+
+    public void PlayLoop(AudioClip clip)
+    {
+        m_sfxLoopSource.loop = true;
+        m_sfxLoopSource.clip = clip;
+        m_sfxLoopSource.Play();
+    }
+
+    public void StopSingle(AudioClip clip)
+    {
+        m_sfxSingleSource.clip = clip;
+        m_sfxSingleSource.Stop();
+    }
+
+    public void StopLoop(AudioClip clip)
+    {
+        m_sfxLoopSource.clip = clip;
+        m_sfxLoopSource.Stop();
     }
 
     public void StartMusic()
