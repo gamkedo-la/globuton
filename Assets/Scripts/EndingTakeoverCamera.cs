1959cfa33c6d748f900b7368a781cd574e94514c
oasisrim <oasisrim@gmail.com>
2017-01-18 19:19:57 -0500

    Paused fly particle system once end cutscene starts. Disabled fly audio source in case it's playing when game ends.

--- Assets/Scripts/EndingTakeoverCamera.cs
+++ Assets/Scripts/EndingTakeoverCamera.cs
@@ -5,6 +5,13 @@ public class EndingTakeoverCamera : MonoBehaviour {
 	public ClickToMove turnOffCharacter;
 	public static bool isPlaying = false;
 
+    private ParticleSystem flies;
+
+    void Start()
+    {
+        flies = GameObject.FindGameObjectWithTag("Flies").GetComponent<ParticleSystem>();
+    }
+
 	// Use this for initialization
 	public void endGame () {
 		isPlaying = true;
@@ -12,6 +19,8 @@ public class EndingTakeoverCamera : MonoBehaviour {
         //Camera.main.transform.position = transform.position;
         //Camera.main.transform.rotation = transform.rotation;
 		gameObject.GetComponent<Cutscene>().enabled = true;
+        flies.Pause(); // Pause flies particle system
+        SoundManager.instance.m_sfxLoopSource.enabled = false;
     }
 	
 }
---
+++ Assets/Scripts/StopFlyAnimation.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StopFlyAnimation : MonoBehaviour
+{
+    [SerializeField]
+    private ParticleSystem flies;
+
+    void Start()
+    {
+        flies = GameObject.FindGameObjectWithTag("Flies").GetComponent<ParticleSystem>();
+    }
+
+    void OnTriggerEnter()
+    {
+        Debug.Log("Flies should be stopped");
+        flies.Pause();
+    }
+}
+
